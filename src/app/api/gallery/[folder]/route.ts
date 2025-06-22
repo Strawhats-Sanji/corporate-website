ab5bd0d431e156b7824e99f07d6a6c9f75ec5705
Hubabah Yusuf <strawhats@summitbankng.com>
2025-06-22 15:20:58 +0100

    cloudinary-gallerys

--- src/app/api/gallery/[folder]/route.ts
+++ src/app/api/gallery/[folder]/route.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server";
-import path from "path";
-import fs from "fs";
+// import { NextResponse } from "next/server";
+// import path from "path";
+// import fs from "fs";
 
-export async function GET(req: Request, { params }: { params: { folder: string } }) {
-  const { folder } = params;
-  const folderPath = path.join(process.cwd(), "public", "gallery", folder);
+// export async function GET(req: Request, { params }: { params: { folder: string } }) {
+//   const { folder } = params;
+//   const folderPath = path.join(process.cwd(), "public", "gallery", folder);
 
-  try {
-    const files = fs.readdirSync(folderPath);
-    const images = files.filter((file) => /\.(png|jpe?g|svg|webp)$/.test(file));
-    return NextResponse.json(images);
-  } catch (error) {
-    return NextResponse.json({ error: "Folder not found" }, { status: 404 });
-  }
-}
+//   try {
+//     const files = fs.readdirSync(folderPath);
+//     const images = files.filter((file) => /\.(png|jpe?g|svg|webp)$/.test(file));
+//     return NextResponse.json(images);
+//   } catch (error) {
+//     return NextResponse.json({ error: "Folder not found" }, { status: 404 });
+//   }
+// }
